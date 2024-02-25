a74d7d37d786a0969355a3a6c597bac0a0253287
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2024-02-25 17:53:00 -0500

    Arreglado el grid de Information Input.

--- src/components/information-input/InformationInput.jsx
+++ src/components/information-input/InformationInput.jsx
@@ -1,6 +1,7 @@
 import './InformationInput.css'
 import { useState } from 'react';
 import { Toolbar } from 'primereact/toolbar';
+import { Dropdown } from 'primereact/dropdown';
 import LabelInput from '../label-input/LabelInput';
 
 const InformationInput = () => {
@@ -19,20 +20,24 @@ const InformationInput = () => {
     const [propertyInsuranceRate, setPropertyInsuranceRate] = useState(0);
     const [postage, setPostage] = useState(0);
 
+    const coinOptions = [
+        ['Soles', 'Dólares'],
+        ['soles', 'dollars']
+    ]
+
     let information = [
-        ['coins', 'Moneda', coins, setCoins],
-        ['disbursementDate', 'Fecha de Desembolso del Crédito', disbursementDate, setDisbursementDate],
-        ['paymentDay', 'Día de Pago', paymentDay, setPaymentDay],
-        ['amount', 'Importe Desembolsado', amount, setAmount],
-        ['propertyValue', 'Valor del Inmueble', propertyValue, setPropertyValue],
-        ['TEA', 'Tasa Efectiva Anual', TEA, setTEA],
-        ['feesPerYear', 'Cuotas al Año', feesPerYear, setFeesPerYear],
-        ['gracePeriod', 'Período de Gracia', gracePeriod, setGracePeriod],
-        ['paymentPeriod', 'Período de Pago', paymentPeriod, setPaymentPeriod],
-        ['totalTerm', 'Plazo Total', totalTerm, setTotalTerm],
-        ['desgravamenInsuranceRate', 'Tasa Seguro de Desgravamen', desgravamenInsuranceRate, setDesgravamenInsuranceRate],
-        ['propertyInsuranceRate', 'Tasa Seguro del Inmueble', propertyInsuranceRate, setPropertyInsuranceRate],
-        ['postage', 'Portes', postage, setPostage]
+        ['disbursementDate', 'Fecha de Desembolso del Crédito', disbursementDate, setDisbursementDate, '/^[^<>*!]+$/'],
+        ['paymentDay', 'Día de Pago', paymentDay, setPaymentDay, 'pint'],
+        ['amount', 'Importe Desembolsado', amount, setAmount, 'pnum'],
+        ['propertyValue', 'Valor del Inmueble', propertyValue, setPropertyValue, 'pnum'],
+        ['TEA', 'Tasa Efectiva Anual', TEA, setTEA, 'pnum'],
+        ['feesPerYear', 'Cuotas al Año', feesPerYear, setFeesPerYear, 'pint'],
+        ['gracePeriod', 'Período de Gracia', gracePeriod, setGracePeriod, 'pint'],
+        ['paymentPeriod', 'Período de Pago', paymentPeriod, setPaymentPeriod, 'pint'],
+        ['totalTerm', 'Plazo Total', totalTerm, setTotalTerm, 'pint'],
+        ['desgravamenInsuranceRate', 'Tasa Seguro de Desgravamen', desgravamenInsuranceRate, setDesgravamenInsuranceRate, 'pnum'],
+        ['propertyInsuranceRate', 'Tasa Seguro del Inmueble', propertyInsuranceRate, setPropertyInsuranceRate, 'pnum'],
+        ['postage', 'Portes', postage, setPostage, 'pnum']
     ]
 
     let userNameH1 = (
@@ -49,10 +54,14 @@ const InformationInput = () => {
     return (
         <div className='information-input'>
             <Toolbar className='toolbar' start={userNameH1} end={userIcon}/>
+            <h1>Insertar Información</h1>
             <div className="information-input-content">
-                <h1>Insertar Información</h1>
+                <div className='input-currency'>
+                    <label htmlFor="currency">Moneda</label>
+                    <Dropdown id="currency" value={coins} onChange={(e) => setCoins(e.value)} options={coinOptions[0]} placeholder='Selecciona una moneda' />
+                </div>
                 {information.map((info) =>
-                    <LabelInput key={info[0]} id={info[0]} text={info[1]} state={info[2]} setState={info[3]} />
+                    <LabelInput key={info[0]} id={info[0]} text={info[1]} state={info[2]} setState={info[3]} keyfilter={info[4]} />
                 )}
             </div>
             <Toolbar className='footer' center='© 2024 Erick Urbizagastegui - Salvador Torres'/>
--- src/components/label-input/LabelInput.jsx
+++ src/components/label-input/LabelInput.jsx
@@ -1,11 +1,11 @@
 import './LabelInput.css'
 import { InputText } from 'primereact/inputtext';
 
-const LabelInput = ({ id, text, state, setState}) => {
+const LabelInput = ({ id, text, state, setState, keyfilter}) => {
     return (
         <div className='label-input'>
             <label htmlFor={id}>{text}</label>
-            <InputText id={id} placeholder={text} value={state} onChange={(e) => setState(e.target.value)}/>
+            <InputText id={id} placeholder={text} value={state} onChange={(e) => setState(e.target.value) } keyfilter={keyfilter}/>
         </div>
     )
 }
